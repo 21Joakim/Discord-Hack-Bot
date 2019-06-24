f266080111b01009f4d4ef7dd07431b4f2f63ed9
Joakim <21Joakim@users.noreply.github.com>
2019-06-24 20:43:06 +0200

    Added ping command

--- Bot/src/main/java/com/jsb/bot/module/ModuleBasic.java
+++ Bot/src/main/java/com/jsb/bot/module/ModuleBasic.java
@@ -1,10 +1,16 @@
 package com.jsb.bot.module;
 
+import com.jockie.bot.core.command.Command;
+import com.jockie.bot.core.command.impl.CommandEvent;
 import com.jockie.bot.core.module.Module;
 
 @Module
 public class ModuleBasic {
 	
-	
-	
+	@Command(description="Shows my ping to Discord")
+	public void ping(CommandEvent event) {
+		event.getJDA().getRestPing().queue(ping -> {
+			event.reply(":stopwatch: **" + ping + "ms**\n:heartbeat: **" + event.getJDA().getGatewayPing() + "ms**").queue();
+		});
+	}
 }
